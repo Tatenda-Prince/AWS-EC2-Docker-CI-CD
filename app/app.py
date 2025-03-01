1a82c2da55ab42c84a8a05a7ce8b4ba0a4bb55f1
Tatenda Prince Moyo <tatendamoyo539@gmail.com>
2025-03-01 13:58:54 +0200

    update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Welcome To Up The Chelsea Tech! This is a Flask app running on AWS EC2!"
+    return "Hello, Welcome To Up The Chelsea Tech! This is a Flask app running on AWS EC2!\nDeployed with GitHub Actions"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
