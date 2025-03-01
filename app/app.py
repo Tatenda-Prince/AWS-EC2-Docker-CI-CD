21c6d74cb54331864c126548d791cf0bb118eae8
Tatenda Prince Moyo <tatendamoyo539@gmail.com>
2025-03-01 19:25:54 +0200

    updated the homepage Removed i love docker

--- app/app.py
+++ app/app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Welcome To Up The Chelsea Tech! This is a Flask app running on AWS EC2!\nI love learning Docker!"
+    return """Hello, Welcome To Up The Chelsea Tech!  
+This is a Flask app running on AWS EC2!    
+Deployed using GitHub Actions!"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
