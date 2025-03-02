f3e48ca2f5047caede5f68a32a06ca6390f87f6e
Tatenda Prince Moyo <tatendamoyo539@gmail.com>
2025-03-02 09:48:25 +0200

    Updated Flask app Homepage

--- app/app.py
+++ app/app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 def home():
     return """Hello, Welcome To Up The Chelsea Tech!  
 This is a Flask app running on AWS EC2!    
-Deployed using GitHub Actions!"""
+Deployed using GitHub Actions!
+Learning Docker is fun!"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
